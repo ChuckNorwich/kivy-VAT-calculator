669a890f491037765fa81e553b582983dbbc5a25
ChuckNorwich <ronmcmillan1971@gmail.com>
2020-05-30 16:41:12 +0100

    Add files via upload

--- VAT_Calculator.py
+++ VAT_Calculator.py
@@ -7,22 +7,10 @@ Config.set('graphics', 'height', MAX_SIZE[1])
 
 from kivy.app import App
 
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from  kivy.uix.button import Button
-from  kivy.uix.widget import Widget
-from  kivy.properties import ObjectProperty
-from kivy.uix.floatlayout import FloatLayout
+
 from kivy.uix.widget import Widget
 from  kivy.properties import ObjectProperty
-from kivy.graphics import Rectangle
-from kivy.graphics import Color
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.popup import Popup
-from plyer import gps
-from kivy.clock import Clock, mainthread
+
 
 
 class Gui(Widget):
